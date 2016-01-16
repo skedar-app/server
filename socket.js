d385eba89dd63c360488c2d189ffd6f1b9aefc24
rémi  <remirobert33530@gmail.com>
2016-01-16 21:07:18 +0800

    add disconnection client when upload is over

--- socket.js
+++ socket.js
@@ -5,21 +5,31 @@ var io = require('socket.io')();
 module.exports = function(server) {
   io.on('connection', function(socket){
     console.log("client connected");
-    socket.on('disconnect', disconnectSocket);
-    ss(socket).on('file', uploadFile);
+    socket.on('disconnect', function() {
+      disconnectSocket()
+    });
+    ss(socket).on('file', function(stream) {
+      uploadFile(socket, stream)
+    });
   });
   io.listen(server);
+}
 
-  var disconnectSocket = function() {
-    console.log("client disconnected");
-  }
+var disconnectSocket = function() {
+  console.log("client disconnected");
+}
 
-  var uploadFile = function(stream) {
-    console.log("get stream upload");
-    var path = './files/' + 'file.txt';
-    stream.pipe(fs.createWriteStream(path));
-    stream.on('data', function(data) {
-      io.emit('uploaded-size', data.length);
-    });
-  }
+var uploadFile = function(socket, stream) {
+  console.log("get stream upload");
+  var path = 'file.txt';
+  var writeStream = fs.createWriteStream(path)
+  stream.pipe(writeStream);
+  stream.on('data', function(data) {
+    console.log(data);
+    io.emit('uploaded-size', data.length);
+  });
+  writeStream.on('finish', function(){
+    console.log("upload over");
+    socket.disconnect();
+  });
 }
